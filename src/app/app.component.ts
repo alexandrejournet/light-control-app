67138da73adc07fdfd7bf2e08aecfade746646a0
Alexandre JOURNET <alexandre.journet@gmail.com>
2022-09-20 21:21:11 +0200

    test pwa updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostListener, OnInit} from '@angular/core';
+import {SwUpdate, VersionEvent} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,9 @@ export class AppComponent implements OnInit {
 
   isPortrait = false;
 
+  constructor(private readonly swUpdate: SwUpdate) {
+  }
+
   @HostListener('window:orientationchange', ['$event'])
   onOrientationChange() {
     this.checkState();
@@ -17,12 +21,29 @@ export class AppComponent implements OnInit {
 
   checkState() {
     this.isPortrait = window.innerHeight > window.innerWidth;
-    console.log(this.isPortrait)
   }
 
   ngOnInit(): void {
     this.checkState();
+    this.swUpdate.available.subscribe((event) => {
+      console.log('current version: ', event.current);
+      console.log('available version: ', event.available);
+      if (confirm('Software update avaialble.')) {
+        this.swUpdate.activateUpdate().then(() => {
+          //Perform your action here
+          window.location.reload()
+        });
+      }
+    });
+
+    this.swUpdate.versionUpdates.subscribe((value: VersionEvent) => {
+      console.log('Available version: ', value);
+      if (confirm('Une nouvelle version est disponible. Votre page va être rechargée.')) {
+        this.swUpdate.activateUpdate().then(() => {
+          //Perform your action here
+          window.location.reload()
+        });
+      }
+    })
   }
-
-
 }
